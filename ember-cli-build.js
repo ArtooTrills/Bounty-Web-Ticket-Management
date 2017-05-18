c001f577133a3fa3caf1af424117c6af11c30df4
Arun Prasad <arun@arunprasad.info>
2017-05-18 22:41:06 +0530

    (feat) New Build Operation - Generate Image Sprites. Add UI Transitions. Dashboard UI enhancements

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,31 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  var mergeTrees = require('broccoli-merge-trees');
+  var broccoliSprite = require('broccoli-sprite');
+  var spritesTree = broccoliSprite('public', {
+    src: ['assets/images/sprites/*.png'],
+    spritePath: 'assets/sprites.png',
+    stylesheetPath: 'assets/sprites.css',
+    stylesheet: 'css',
+    stylesheetOptions: {
+      prefix: 'sprite-',
+      spritePath: '/assets/sprites.png',
+    },
+    layout: 'packed',
+    //compositior: https://github.com/selaux/node-sprite-generator#optionscompositor
+    compositor: 'gm', //'canvas | gm | jimp'
+    // optimize png using optiPNG
+    // This adds considerable build time, but can drastically reduce your sprite file size.
+    // optiping: (process.env.NODE_ENV === 'production'),
+  });
 
-  return app.toTree();
+  // merge appTree and spritesTree
+  var appTree = app.toTree();
+  var mergedTree = mergeTrees([appTree, spritesTree], {
+    overwrite: true
+  });
+
+  // return app.toTree();
+  return mergedTree;
 };
